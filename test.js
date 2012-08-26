ed72f85416337587c7d257cf09651419e74b81d7
Bo Han <bo.ning.han@gmail.com>
2012-08-26 16:48:54 -0400

    removePlaylist working

--- test.js
+++ test.js
@@ -24,11 +24,9 @@ dbConnector.open(function(err, db) {
 			if(err) { console.log(err); return; }
 		});
 		
-		console.log("users collection finished");
 		usersCollection = collection;
 
 		if(playlists && usersCollection) {
-			console.log("users collection last");			
 			run();
 		}
 	});
@@ -40,11 +38,9 @@ dbConnector.open(function(err, db) {
 			return;
 		}
 		
-		console.log("playlists collection finished");
 		playlists = collection;
 
 		if(usersCollection && playlists) {
-			console.log("playlists collection last");			
 			run();
 		}
 	});
@@ -78,14 +74,12 @@ function writeSuccess(res, value) {
 
 // creates a new playlist for a given user
 function addPlaylist(req, res, next) {
-	/*if(!req['user']) {
+	if(!req['user']) {
 		writeError(res, "no user given"); return;	
 	}
 	var userID = new ObjectID(req['user']);
-	if(userID == null) { writeError(res, "no userID"); return;}
-	*/
+	if(!userID) { writeError(res, "no userID"); return;}
 	
-	var userId = req['userId'];
 	/*var query = getQueries(req);
 
 	if(!query['playlistName']) {
@@ -95,10 +89,11 @@ function addPlaylist(req, res, next) {
 	*/
 	var playlistName = req['playlistName'];
 	
-	var playlist = new Object();
-	playlist['name'] = playlistName;
-	playlist['owner'] = userId;
-	playlist['videos'] = new Array();
+	var playlist = {
+		'name': playlistName,
+		'owner': userID,
+		'videos': new Array()
+	};
 	
 	// inserts a playlist into the playlists collection
 	// updates the user's info with a reference to the inserted playlist
@@ -113,7 +108,7 @@ function addPlaylist(req, res, next) {
 
 			// update with reference to playlists in user doc
 			usersCollection.update(
-				{'_id': userId},
+				{'_id': userID},
 				{'$push': {'playlists': playlistRef}}, 
 				function(err, count) {
 					if(err) { 
@@ -131,7 +126,7 @@ function addPlaylist(req, res, next) {
 	// otherwise insert the playlist
 	// is this fast enough?
 	playlists.findOne(
-		{'owner': userId, 'name': playlistName}, 
+		{'owner': userID, 'name': playlistName}, 
 		function(err, doc) {
 			if(doc) { 
 				writeError(res, "playlist \""+playlistName+"\" exists already");
@@ -143,51 +138,76 @@ function addPlaylist(req, res, next) {
 	);
 }
 
-// returns user's favorites playlist if it exists
-function getFavorite(userId) {
-	usersCollection.findOne({'_id': userId}, function(err, doc) {
-		if(err) { return {'error': err}; }
-		if(!doc) { return {'error': 'No such user'}; }
-		
-		// if favID is defined, return the favorites playlist
-		// else create the favorites playlist for the user
-		if(doc['favId']) {
+// remove a given playlist for a given user
+function removePlaylist(req, res, next) {
+	if(!req['user']) {
+		writeError(res, "no user given"); return;	
+	}
+	var userID = new ObjectID(req['user']);
+	if(!userID) { writeError(res, "no userID"); return;}
 
-		} else {
-			var favorites = {
-				'name': "Favorites",
-				'owner': userId,
-				'videos': new Array()
-			};
+	/*var query = getQueries(req);
 
-			playlists.insert(favorites, function(err, result) {
-				if(err) { return {'error': err}; }
-
-				// update with reference to favorites in user doc
-				usersCollection.update(
-					{'_id': userId},
-					{'$set': {'favId': result[0]['_id']}}, 
-					function(err, count) {
-						if(err) { return {'error': err}; }
-						
-						return {
-							'name': 'Favorites',
-							'_id': result[0]['_id']
-						};
-					}
-				);
-			});
+	if(!query['playlistName']) {
+		writeError(res, "no playlistName given"); return;
+	}
+	var playlistName = query['playlistName'];
+	*/
+	var playlistID = new ObjectID(req['playlistID']);
+	
+	// find the user doc, check that playlistID is not the favorites
+	// remove the playlist from playlists
+	// remove the reference to the playlist from the user doc
+	usersCollection.findOne({'_id': userID}, function(err, doc) {
+		if(err) { writeError(res, "error finding user document"); return; }
+		if(!doc) {
+			writeError(res, "user doesn't exist in system"); 
+			return; 
+		}		
+		// error if trying to delete Favorites playlist
+		if(doc['favId'] == userID) {
+			writeError(res, "cannot delete Favorites playlist"); return;
 		}
+		
+		// remove playlist from playlists for user
+		playlists.remove(
+			{'_id': playlistID, owner: userID}, 
+			function(err, result) {
+				if(err) { writeError(res, "error removing playlist"); return;}
+				
+				var pos = -1;			
+				var playlistRefs = doc['playlists'];
+
+				for(var i = 0; i < playlistRefs.length; i++) {
+					console.log(playlistRefs[i]['_id']);
+					if(playlistRefs[i]['_id'].equals(playlistID)) {
+						pos = i; break;
+					}
+				}
+
+				// found a reference to the playlist in the user doc
+				if(pos != -1) {
+					playlistRefs.splice(pos, 1);
+					usersCollection.update(
+						{'_id': userID},
+						{'$set': {'playlists': playlistRefs}}, 
+						function(err, count) {
+							if(err) { return {'error': err}; }
+						
+							writeSuccess(res);
+						}
+					);
+				}
+			}
+		);
 	});
-}
-
-// returns the user's favorites and other playlists
-function getPlaylists() {
-
+	
 }
 
 function run() {
-	var userId = new ObjectID("50329ad3ac6815bf24000001");
-	//getFavorite(userId);
-	addPlaylist({'userId': userId, 'playlistName': '0'});
+	var user = "50329ad3ac6815bf24000001";
+	var playlistID = "503a8b6d4f286ebf38000001";
+	//addPlaylist({'user': user, 'playlistName': '1'});
+	removePlaylist({'user': user, 'playlistID': playlistID});
 }
+
